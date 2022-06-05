ff6fe5c5f052f38909c402aae36d3ba1cbc28fcb
Marmar <71787768+AmarMsd@users.noreply.github.com>
2022-06-05 22:13:10 +0700

    Add-Default-Style

--- src/scripts/views/template/wisata-template.js
+++ src/scripts/views/template/wisata-template.js
@@ -1,12 +1,11 @@
 const createWisataItemTemplate = (content) => `
-    <div class="wisata" tabindex=0>
+    <div class="card" tabindex="0">
       <a href="#/wisata/${content.id}" class="wisata-link">
         <div class="img-wisata">
           <img tabindex="0" src="${content.picture}" class="wisata_image" alt="${content.name}"/>
         </div>
 
-        <div class="umkm-desc" tabindex="0">
-          <span>${content.descripton}</span></p>
+        <div class="wisata-desc" tabindex="0">
           <h1 class="card-content-title">${content.name}</h1>
           <p class="truncate">${content.description}</p>
         </div>
