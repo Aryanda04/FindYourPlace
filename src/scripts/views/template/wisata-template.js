05a434043f413999b22fbfa5fcefd602308ae072
Marmar <71787768+AmarMsd@users.noreply.github.com>
2022-06-05 22:36:16 +0700

    Add css

--- src/scripts/views/template/wisata-template.js
+++ src/scripts/views/template/wisata-template.js
@@ -1,13 +1,13 @@
 const createWisataItemTemplate = (content) => `
-    <div class="card" tabindex="0">
+    <div class="card-wisata" tabindex="0">
       <a href="#/wisata/${content.id}" class="wisata-link">
         <div class="img-wisata">
           <img tabindex="0" src="${content.picture}" class="wisata_image" alt="${content.name}"/>
         </div>
 
         <div class="wisata-desc" tabindex="0">
-          <h1 class="card-content-title">${content.name}</h1>
-          <p class="truncate">${content.description}</p>
+          <h1 class="card-content-title-wisata">${content.name}</h1>
+          <p class="truncate-wisata">${content.description}</p>
         </div>
       <a>
     <div>
