1cfc178266ea4745d4252190794d32c47f49da2b
Marmar <71787768+AmarMsd@users.noreply.github.com>
2022-06-07 01:01:28 +0700

    add data

--- src/scripts/views/template/wisata-template.js
+++ src/scripts/views/template/wisata-template.js
@@ -1,6 +1,6 @@
 const createWisataItemTemplate = (content) => `
     <div class="card-wisata" tabindex="0">
-      <a href="#/wisata/${content.id}" class="wisata-link">
+      <a href="#/detail-wisata/${content.id}" class="wisata-link">
         <div class="img-wisata">
         
           <img tabindex="0" src="${content.picture}" class="wisata_image" alt="${content.name}"/>
