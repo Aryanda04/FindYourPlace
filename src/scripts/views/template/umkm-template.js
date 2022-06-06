651be96b7f7853120d1e92b36c39f765fa1ce76d
Tiyas <tiiyasastutiningsih@gmail.com>
2022-06-06 22:58:08 +0700

    add data wisata

--- src/scripts/views/template/umkm-template.js
+++ src/scripts/views/template/umkm-template.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 const createUmkmItemTemplate = (conten) => `
     <div class="umkm" tabindex="0">
-      <a href="#/umkm/${conten.id}" class="umkm-link">
+      <a href="#/detail-umkm/${conten.id}" class="umkm-link">
         <div class="img-umkm"> 
           <img tabindex="0" src="${conten.picture}" class="umkm_image" alt="${conten.name}"/>
         </div>
@@ -18,7 +18,7 @@ const createBantenItemTemplate = (conten) => `
 <img tabindex="0" src="${conten.picture}" class="umkm_image" alt="${conten.name}"/>
 <div class="umkm-desc" tabindex="0" >
   <h1 class="card-content-title">${conten.name}</h1>
-  <a href="#/umkm/${conten.id}" class="umkm-link">
+  <a href="#/detail-umkm/${conten.id}" class="umkm-link">
 </div>
 </div>
   `;
