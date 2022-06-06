0a5c41d6f59076204b2305994d6a252d73467392
Marmar <71787768+AmarMsd@users.noreply.github.com>
2022-06-06 22:57:41 +0700

    Update data travel

--- src/scripts/views/template/wisata-template.js
+++ src/scripts/views/template/wisata-template.js
@@ -2,6 +2,7 @@ const createWisataItemTemplate = (content) => `
     <div class="card-wisata" tabindex="0">
       <a href="#/wisata/${content.id}" class="wisata-link">
         <div class="img-wisata">
+        
           <img tabindex="0" src="${content.picture}" class="wisata_image" alt="${content.name}"/>
         </div>
 
