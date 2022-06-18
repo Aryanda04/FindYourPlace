d05ee727e150f318382cd7e53a05dc3a0bcb9811
Aryanda Rizky P <aryandarizky1234@gmail.com>
2022-06-19 01:47:24 +0700

    add templateting video, instagram, maps

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,28 +1,31 @@
-import "regenerator-runtime";
-import "../styles/style.css";
-import "../styles/jumbotron.css";
-import "../styles/footer.css";
-import "../styles/home.css";
-import "../styles/umkm.css";
-import "../styles/budaya.css";
-import "../styles/wisata.css";
-import App from "./views/app";
+import 'regenerator-runtime';
+import '../styles/style.css';
+import '../styles/jumbotron.css';
+import '../styles/footer.css';
+import '../styles/home.css';
+import '../styles/umkm.css';
+import '../styles/budaya.css';
+import '../styles/wisata.css';
+import App from './views/app';
 
-import "./views/component/custom-footer";
-import "./views/component/custom-navbar";
-import "./views/component/custom-jumbotron";
+import './views/component/custom-footer';
+import './views/component/custom-navbar';
+import './views/component/custom-jumbotron';
 
 const app = new App({
-  button: document.querySelector("#hamburgerButton"),
-  drawer: document.querySelector("#navigationDrawer"),
-  content: document.querySelector("#mainContent"),
+  button: document.querySelector('#hamburgerButton'),
+  drawer: document.querySelector('#navigationDrawer'),
+  content: document.querySelector('#mainContent'),
 });
-window.addEventListener("hashchange", () => {
+window.addEventListener('hashchange', () => {
   app.renderPage();
 });
 
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   app.renderPage();
+  const ig = document.createElement('script');
+  ig.src = '//www.instagram.com/embed.js';
+  document.body.appendChild(ig);
 });
 
 // const menu = document.querySelector('#menu');
