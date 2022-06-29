a60f896d3d0400051f16c4686c7376b7fe2e5aac
Aryanda Rizky P <aryandarizky1234@gmail.com>
2022-06-29 19:51:32 +0700

    clear comment

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -27,21 +27,3 @@ window.addEventListener('load', () => {
   ig.src = '//www.instagram.com/embed.js';
   document.body.appendChild(ig);
 });
-
-// const menu = document.querySelector('#menu');
-// const hero = document.querySelector('.hero');
-// const main = document.querySelector('main');
-// const drawer = document.querySelector('#drawer');
-
-// menu.addEventListener('click', (event) => {
-//   drawer.classList.toggle('open');
-//   event.stopPropagation();
-// });
-
-// hero.addEventListener('click', () => {
-//   drawer.classList.remove('open');
-// });
-
-// main.addEventListener('click', () => {
-//   drawer.classList.remove('open');
-// });
